fb8c639bc81b9fb36b51886beb20da411acfbcc8
rumeysa.yuk <rumeysayuk90@gmail.com>
2022-07-03 01:16:59 +0300

    Components created and navbar,sidebar

---
+++ client/src/components/Charts/LineChart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LineChart = () => {
+   return (
+      <div>
+         line chart
+      </div>
+   )
+}
+
+export default LineChart;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client';
-import  App  from './App';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import "./index.css"
+import {ContextProvider} from "./contexts/ContextProvider";
+
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(<ContextProvider><App tab="home"/></ContextProvider>);
