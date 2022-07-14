fb8fe6f8b28de084969d8db1846ce3ab32b812ae
rumeysa.yuk <rumeysayuk90@gmail.com>
2022-07-14 23:31:21 +0300

    models & pdf upload & multer upload

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,21 @@ const authRoutes = require("./authRoutes")
 const {create, update, getAll, deleteData, getById} = require("../controllers/baseController");
 const {getAccessToRoute} = require("../middlewares/authorization/auth");
 const router = express.Router();
+const upload = require("../middlewares/lib/multerUpload")
+const pdfUpload = require("../middlewares/lib/pdfUpload");
+
+router.use("/upload", upload.array("images"), (req, res, next) => {
+   res.status(200).json({
+      urls: (req.savedImages || []).map(image => process.env.HOST + "/" + image),
+      message: "Image upload successfully",
+   })
+})
+router.use("/pdfupload", pdfUpload.single("file"), (req, res, next) => {
+   res.status(200).json({
+      url: process.env.HOST + "/" + req.savedImage,
+      message: "Successfully uploaded"
+   })
+})
 
 router.use("/auth", authRoutes);
 router.post("/base/:service", getAccessToRoute, create)
