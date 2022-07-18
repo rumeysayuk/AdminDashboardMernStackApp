21384bd1e88e499ea1b2a90eb8b5ecba5e7edc87
rumeysa.yuk <rumeysayuk90@gmail.com>
2022-07-19 00:35:05 +0300

    .

--- client/src/pages/ProfileDetail.js
+++ client/src/pages/ProfileDetail.js
@@ -8,143 +8,153 @@ import {useNavigate} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider";
 
 const ProfileDetail = () => {
-    const {authData} = useSelector(state => state.auth)
-    const {token} = useSelector(state => state.auth)
-    const [profile, setProfile] = useState({...authData})
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const {currentColor} = useStateContext();
-    let userId = authData?._id
+   const {authData} = useSelector(state => state.auth)
+   const {token} = useSelector(state => state.auth)
+   const [profile, setProfile] = useState({...authData})
+   const dispatch = useDispatch()
+   const navigate = useNavigate()
+   const {currentColor} = useStateContext();
+   let userId = authData?._id
 
-    const Msg = ({closeToast, toastProps}) => (
-        <div>
-            Lorem ipsum dolor {toastProps.position}
-            <button>Retry</button>
-            <button onClick={closeToast}>Close</button>
-        </div>
-    )
-    const displayMsg = () => {
-        toast(<Msg/>)
-        // toast(Msg) would also work
-    }
+   const Msg = ({closeToast, toastProps}) => (
+      <div>
+         Lorem ipsum dolor {toastProps.position}
+         <button>Retry</button>
+         <button onClick={closeToast}>Close</button>
+      </div>
+   )
+   const displayMsg = () => {
+      toast(<Msg/>)
+      // toast(Msg) would also work
+   }
 
-    const deleteAccount = () => {
-        apiAxios.put(`/base/User/${profile._id}`, {data: {isBlocked: !profile.isBlocked}}).then(() => {
-            toast.success("Deleted Account")
-            setTimeout(() => {
-                localStorage.removeItem("token")
-                window.location.href = "/"
-            }, 1000)
-        })
-    }
+   const deleteAccount = () => {
+      apiAxios.put(`/base/User/${profile._id}`, {data: {isBlocked: !profile.isBlocked}}).then(() => {
+         toast.success("Deleted Account")
+         setTimeout(() => {
+            localStorage.removeItem("token")
+            window.location.href = "/"
+         }, 1000)
+      })
+   }
 
-    const save = (e) => {
-        e.preventDefault()
-        apiAxios.put(`/base/User/${userId}`, {data: profile}).then(() => {
-            dispatch(handleAuth({authData: profile, token: token}))
-            displayMsg()
-        }).catch(e => {
-            console.log(e)
-        })
-    }
+   const save = (e) => {
+      e.preventDefault()
+      apiAxios.put(`/base/User/${userId}`, {data: profile}).then(() => {
+         dispatch(handleAuth({authData: profile, token: token}))
+         displayMsg()
+      }).catch(e => {
+         console.log(e)
+      })
+   }
 
 
-    useEffect(() => {
-        setTimeout(() => {
-            if (!token) {
-                toast.info("PleaseFirstLogin")
-                navigate("/auth")
-            }
-        }, 1000)
-    }, [])
+   useEffect(() => {
+      setTimeout(() => {
+         if (!token) {
+            toast.info("PleaseFirstLogin")
+            navigate("/auth")
+         }
+      }, 1000)
+   }, [])
 
-    if (!profile || !authData) return <div
-        className="flex w-full justify-center align-middle h-full">Loading...</div>;
-    return (
-        <div className="container mx-auto">
-            <div>
-                <div>
-                    <div className="text-center mt-20 mb-4 ml-5 dark:text-gray-300"
-                         style={{color: currentColor}}>Welcome
-                        Profile {`${(authData.name || "")} ${(authData.lastname || "")}`}</div>
-                </div>
+   if (!profile || !authData) return <div
+      className="flex w-full justify-center align-middle h-full">Loading...</div>;
+   return (
+      <div className="container mx-auto">
+         <div>
+            <div className={"flex justify-between align-middle flex-row"}>
+               <div className="text-center mt-20 mb-4 ml-5 dark:text-gray-300"
+                    style={{color: currentColor}}>Welcome
+                  Profile {`${(authData.name || "")} ${(authData.lastname || "")}`}
+               </div>
+                  {/*<button type="submit" style={{backgroundColor: currentColor, color: "white"}}*/}
+                  {/*        onClick={deleteAccount}*/}
+                  {/*        className="ml-3 h-10 text-md align-middle justify-center flex rounded-lg hover:bg-light-gray">*/}
+                  {/*   Delete my account*/}
+                  {/*</button>*/}
+
             </div>
-            <form>
-                <div className="grid gap-6 mb-6 md:grid-cols-2">
-                    <div>
-                        <label htmlFor="formFile" className="form-label inline-block mb-2 text-gray-700">Profile Image</label>
-                        <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" type="file" id="formFile"/>
-                    </div>
-                    <div>
-                        <label htmlFor="name"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First
-                            name</label>
-                        <input type="text" id="name" defaultValue={authData?.name}
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="John" required
-                               onChange={(e) => setProfile({...profile, name: e.target.value})}/>
-                    </div>
-                    <div>
-                        <label htmlFor="lastname"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last
-                            name</label>
-                        <input type="text" id="lastname" defaultValue={authData?.lastname}
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="Doe" required=""
-                               onChange={(e) => setProfile({...profile, lastname: e.target.value})}/>
-                    </div>
-                    <div>
-                        <label htmlFor="email"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email
-                            address</label>
-                        <input type="email" id="email " defaultValue={authData?.email}
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="ohn.doe@company.com"
-                               onChange={(e) => setProfile({...profile, email: e.target.value})}/>
-                    </div>
-                    <div>
-                        <label htmlFor="phone"
-                               className="block mb-2 text-sm font-medium text-  gray-900 dark:text-gray-300">Phone
-                            number</label>
-                        <input type="tel" id="phone" defaultValue={authData?.phone}
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="123-45-678"
-                               onChange={(e) => setProfile({...profile, phone: e.target.value})}/>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" defaultValue={authData?.password}
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
-                        <input type="password" id="password"
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="•••••••••" required=""
-                               onChange={(e) => setProfile({...profile, password: e.target.value})}/>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="confirm_password"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm
-                            password</label>
-                        <input type="password" id="confirm_password"
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                               placeholder="•••••••••" required=""
-                               onChange={(e) => setProfile({...profile, confirmPassword: e.target.value})}/>
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="join_date"
-                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Joined
-                            Date</label>
-                        <input type="text" id="join_date" defaultValue={moment(authData?.createdAt).format("LLLL")}
-                               disabled
-                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-                    </div>
-                </div>
-                <button type="submit" style={{backgroundColor: currentColor, color: "white"}}
-                        onClick={save}
-                        className="w-full text-md align-middle justify-center flex rounded-lg p-3 hover:bg-light-gray">
-                    Save
-                </button>
-            </form>
-        </div>
-    )
+         </div>
+         <form>
+            <div className="grid gap-6 mb-6 md:grid-cols-2">
+               <div>
+                  <label htmlFor="formFile" className="form-label inline-block mb-2 text-gray-700 dark:text-gray-300">Profile
+                     Image</label>
+                  <input
+                     className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                     type="file" id="formFile"/>
+               </div>
+               <div>
+                  <label htmlFor="name"
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First
+                     name</label>
+                  <input type="text" id="name" defaultValue={authData?.name}
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="John" required
+                         onChange={(e) => setProfile({...profile, name: e.target.value})}/>
+               </div>
+               <div>
+                  <label htmlFor="lastname"
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Last
+                     name</label>
+                  <input type="text" id="lastname" defaultValue={authData?.lastname}
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="Doe" required=""
+                         onChange={(e) => setProfile({...profile, lastname: e.target.value})}/>
+               </div>
+               <div>
+                  <label htmlFor="email"
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email
+                     address</label>
+                  <input type="email" id="email " defaultValue={authData?.email}
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="ohn.doe@company.com"
+                         onChange={(e) => setProfile({...profile, email: e.target.value})}/>
+               </div>
+               <div>
+                  <label htmlFor="phone"
+                         className="block mb-2 text-sm font-medium text-  gray-900 dark:text-gray-300">Phone
+                     number</label>
+                  <input type="tel" id="phone" defaultValue={authData?.phone}
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="123-45-678"
+                         onChange={(e) => setProfile({...profile, phone: e.target.value})}/>
+               </div>
+               <div className="mb-4">
+                  <label htmlFor="password" defaultValue={authData?.password}
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+                  <input type="password" id="password"
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="•••••••••" required=""
+                         onChange={(e) => setProfile({...profile, password: e.target.value})}/>
+               </div>
+               <div className="mb-4">
+                  <label htmlFor="confirm_password"
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm
+                     password</label>
+                  <input type="password" id="confirm_password"
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                         placeholder="•••••••••" required=""
+                         onChange={(e) => setProfile({...profile, confirmPassword: e.target.value})}/>
+               </div>
+               <div className="mb-6">
+                  <label htmlFor="join_date"
+                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Joined
+                     Date</label>
+                  <input type="text" id="join_date" defaultValue={moment(authData?.createdAt).format("LLLL")}
+                         disabled
+                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+               </div>
+            </div>
+            <button type="submit" style={{backgroundColor: currentColor, color: "white"}}
+                    onClick={save}
+                    className="w-full text-md align-middle justify-center flex rounded-lg p-3 hover:bg-light-gray">
+               Save
+            </button>
+         </form>
+      </div>
+   )
 }
 
 export default ProfileDetail;
